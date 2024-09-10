ddb99ee81059e03883a2db3cf202295c34e4b8b5
rohiitgit <rohitcodes03@gmail.com>
2024-09-10 16:37:24 +0530

    user information handling done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { UserButton } from "@/features/auth/components/user-button";
+
+import { useGetWorkspaces } from "@/features/auth/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
 
-  const { signOut } = useAuthActions();
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [workspaceId, isLoading]);
+
   return (
     <div>
-      Logged In!!
-      <Button onClick={() => signOut()}>Sign Out</Button>
+      <UserButton />
     </div>
   )
 }
